23809e590f85539a7975b3cb09630f258f985f75
José lópez Chávez <ayroz.jlc@gmail.com>
2019-12-12 18:49:32 -0600

    add google fonts

--- src/AdminLte3Asset.php
+++ src/AdminLte3Asset.php
@@ -5,17 +5,18 @@ use yii\web\AssetBundle;
 
 class AdminLte3Asset extends AssetBundle
 {
-	public $sourcePath = '@vendor/almasaeed2010/adminlte';
+    public $sourcePath = '@vendor/almasaeed2010/adminlte';
     public $css = [
-        YII_ENV_DEV ? 'dist/css/adminlte.css' : 'dist/css/adminlte.min.css',
-        YII_ENV_DEV ? 'plugins/fontawesome-free/css/all.css' : 'plugins/fontawesome-free/css/all.css'
+        YII_ENV_DEV ? 'plugins/fontawesome-free/css/all.css' : 'plugins/fontawesome-free/css/all.min.css',
+        '//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700',
+        YII_ENV_DEV ? 'dist/css/adminlte.css' : 'dist/css/adminlte.min.css'
     ];
     public $js = [
         YII_ENV_DEV ? 'dist/js/adminlte.js' : 'dist/js/adminlte.min.js'
     ];
 
     public $depends = [
-    	'yii\web\YiiAsset',
+        'yii\web\YiiAsset',
         'yii\bootstrap4\BootstrapPluginAsset',
     ];
 }
