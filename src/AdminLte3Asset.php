99e2b66ad327d03e45bd57b3156832cd3161ba65
José lópez Chávez <ayroz.jlc@gmail.com>
2020-10-06 12:59:16 -0500

    delete font cdn google

--- src/AdminLte3Asset.php
+++ src/AdminLte3Asset.php
@@ -8,7 +8,6 @@ class AdminLte3Asset extends AssetBundle
     public $sourcePath = '@vendor/almasaeed2010/adminlte';
     public $css = [
         YII_ENV_DEV ? 'plugins/fontawesome-free/css/all.css' : 'plugins/fontawesome-free/css/all.min.css',
-        '//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700',
         YII_ENV_DEV ? 'dist/css/adminlte.css' : 'dist/css/adminlte.min.css'
     ];
     public $js = [
